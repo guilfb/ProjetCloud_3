c216161295c01fac2408d30956f458ab5bee62df
guilfb <guillaumelefebvre69@gmail.com>
2020-01-08 11:45:21 +0100

    maj index.js

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const app = express();
+var bodyParser = require('body-parser');
 
 const PORT = 8080;
 
+const users = [
+	{"username": "Gui","lastname": "Lefebvre","firstname": "Guillaume","age": 21},
+	{"username": "Raph","lastname": "Marie-Nelly","firstname": "Raphael","age": 27}
+]
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 app.get("/", (req, res) => {
-	res.send("Bienvenue sur le groupe 3\n")
-}); 
+	res.send(users)
+	console.log(users)
+});
 
 app.listen(PORT, function() {
 	console.log(`Listening on ${PORT}`);
 });
+
 
 /*
 var MongoClient = require('mongodb').MongoClient;
