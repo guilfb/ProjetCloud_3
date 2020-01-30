ea36736003383f991cb29b01352890569e8bb899
guilfb <guillaumelefebvre69@gmail.com>
2020-01-30 08:27:06 +0100

    test of env

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 const express = require('express')
 const mongoose = require('mongoose')
 var bodyParser = require('body-parser')
+//require('dotenv').config({ path: require('find-config')('.env') })
 
-var urldev = 'mongodb://ukkig3mswvf53dj5fgpu:LmD0HD9agInQX19mdMDo@b4k6cnacznjiujv-mongodb.services.clever-cloud.com:27017/b4k6cnacznjiujv'
+// var urldev = 'mongodb://ukkig3mswvf53dj5fgpu:LmD0HD9agInQX19mdMDo@b4k6cnacznjiujv-mongodb.services.clever-cloud.com:27017/b4k6cnacznjiujv'
 // var urlprod = 'mongodb://upzbyeqmkvcoeyg3yqnf:iJnxlOjDRjA8m7vOZOX5@bhirs6eketqjm8b-mongodb.services.clever-cloud.com:27017/bhirs6eketqjm8b'
 
-const PORT = 8080
+let url;
+let env = process.env.environnement || "dev"
+
+if(env == "dev") {
+  url = 'mongodb://ukkig3mswvf53dj5fgpu:LmD0HD9agInQX19mdMDo@b4k6cnacznjiujv-mongodb.services.clever-cloud.com:27017/b4k6cnacznjiujv'
+} else {
+  url = 'mongodb://upzbyeqmkvcoeyg3yqnf:iJnxlOjDRjA8m7vOZOX5@bhirs6eketqjm8b-mongodb.services.clever-cloud.com:27017/bhirs6eketqjm8b'
+}
+
+console.log(env)
+console.log(url)
+
+//const PORT = 8080
+const PORT = '${process.env.PORT || 8080}'
 
 const app = express()
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
 app.use(bodyParser.json({ limit: '50mb', extended: true }))
 
 // Connection to MongoDB
-mongoose.connect(urldev, {
+mongoose.connect(url, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false
